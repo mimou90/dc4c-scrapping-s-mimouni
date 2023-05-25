ed80607f55639e2e35dd5a0b7b95a647718b087a
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-05-25 20:07:32 +0200

    extration 10 premières pages

---
+++ site2.py
@@ -0,0 +1,19 @@
+import requests as r
+from bs4 import BeautifulSoup
+
+url = 'https://www.scrapethissite.com/pages/forms/'
+
+def extraire_pages (url) :
+ response = r.get(url)
+ if response.status_code == 200 :
+  soup = BeautifulSoup(response.content, 'html.parser')
+  for s in range (1,11) :
+   Url_page = f'https://www.scrapethissite.com/pages/forms/?page={s}'
+
+   print (Url_page)
+
+extraire_pages(url)
+response = r.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup)
+
